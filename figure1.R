ac375dbb8dc92638037e71deafc2a244a475d581
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-07-17 16:58:49 +0200

    update with tempfile

--- figure1.R
+++ figure1.R
@@ -5,12 +5,13 @@ offline <- T
 
 version <- "7722817"
 
+temp <- tempfile()
+
 curl_download(paste0("https://www.insee.fr/fr/statistiques/fichier/", version, "/t_men_val.xls"),
-              destfile = "t_men_val.xls",
+              destfile = temp,
               quiet = F)
 
-
-figure1 <- readxl::read_excel("t_men_val.xls", skip = 7, sheet = 2) %>%
+figure1 <- readxl::read_excel(temp, skip = 7, sheet = 2) %>%
   transmute(date = zoo::as.yearqtr(...1, format = "%YT%q"),
             `Intérêts et dividendes nets reçus` = D4,
             `Revenu disponible brut` = B6) %>%
@@ -20,6 +21,8 @@ figure1 <- readxl::read_excel("t_men_val.xls", skip = 7, sheet = 2) %>%
   group_by(variable) %>%
   arrange(date) %>%
   mutate(value = 100*value/value[1])
+
+unlink(temp)
 
 figure1 %>%
   ggplot + geom_line(aes(x = date, y = value, color = variable)) +
@@ -32,7 +35,6 @@ figure1 %>%
   geom_label(data = . %>% 
               filter(date == max(date)),
             aes(x = date, y = value,  color = variable, label = round(value, 1)))
-
 
 ggsave("figure1.png", width = 1.25*6, height = 1.25*3.375)
 ggsave("figure1.pdf", width = 1.25*6, height = 1.25*3.375)
