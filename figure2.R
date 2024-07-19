faa92e7f69f77560e5bb7da1a37b504fe4a8ae80
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-07-19 16:09:00 +0200

    update with tidy functions

--- figure2.R
+++ figure2.R
@@ -14,6 +14,25 @@ version2 <- "8068622"
 # https://www.insee.fr/fr/statistiques/8068612
 version3 <- "8068612"
 
+tidy1 <- function(data){
+  data |>
+    mutate(line = 1:n()) |>
+    rename(variable = ...1, Variable = ...2) |>
+    gather(year, value, -variable, -Variable, -line) |>
+    filter(!is.na(value)) |>
+    mutate(year = as.numeric(year))
+}
+
+tidy2 <- function(data){
+  data |>
+    mutate(line = 1:n()) |>
+    rename(Variable = ...1) |>
+    gather(year, value, -Variable, -line) |>
+    mutate(value = as.numeric(value)) |>
+    filter(!is.na(value)) |>
+    mutate(year = as.numeric(year))
+}
+
 # PIB --------
 
 temp <- tempfile()
@@ -22,11 +41,7 @@ curl::curl_download(paste0(url_insee, version1, "/T_1101_1103.xlsx"),
                     temp)
 
 T_1101 <- read_excel(temp, sheet = "T_1101 en niveau", skip = 3) |>
-  mutate(line = 1:n()) |>
-  rename(variable = ...1, Variable = ...2) |>
-  gather(year, value, -variable, -Variable, -line) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy1()
 
 PIB <- T_1101 |>
   filter(variable == "B1GQ") |>
@@ -42,11 +57,7 @@ curl::curl_download(paste0(url_insee, version1, "/T_1101_1103.xlsx"),
                     temp)
 
 T_1103 <- read_excel(temp, sheet = "T_1103 en évolution", skip = 3) |>
-  mutate(line = 1:n()) |>
-  rename(variable = ...1, Variable = ...2) |>
-  gather(year, value, -variable, -Variable, -line) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy1()
 
 deflateur <- T_1103 |>
   filter(variable == "B1GQ") |>
@@ -62,11 +73,7 @@ curl::curl_download(paste0(url_insee, version1, "/T_1101_1103.xlsx"),
                     temp)
 
 T_1102 <- read_excel(temp, sheet = "T_1102 en évolution", skip = 3) |>
-  mutate(line = 1:n()) |>
-  rename(variable = ...1, Variable = ...2) |>
-  gather(year, value, -variable, -Variable, -line) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy1()
 
 croissance_reelle <- T_1102 |>
   filter(variable == "B1GQ") |>
@@ -82,11 +89,7 @@ curl::curl_download(paste0(url_insee, version2, "/t_3101.xlsx"),
                     temp)
 
 t_3101 <- read_excel(temp, skip = 2) |>
-  mutate(line = 1:n()) |>
-  rename(Variable = ...1) |>
-  gather(year, value, -Variable, -line) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy2()
 
 dette_PIB <- t_3101 |>
   filter(line == 10) |>
@@ -102,12 +105,7 @@ curl::curl_download(paste0(url_insee, version2, "/t_3106.xlsx"),
                     temp)
 
 t_3106 <- read_excel(temp, skip = 1) |>
-  mutate(line = 1:n()) |>
-  rename(Variable = ...1) |>
-  gather(year, value, -Variable, -line) |>
-  mutate(value = as.numeric(value)) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy2()
 
 deficit_PIB <- t_3106 |>
   filter(line == 10) |>
@@ -123,12 +121,7 @@ curl::curl_download(paste0(url_insee, version3, "/T_7301.xlsx"),
                     temp)
 
 T_7301 <- read_excel(temp, skip = 4, sheet = 2) |>
-  mutate(line = 1:n()) |>
-  rename(variable = ...1, Variable = ...2) |>
-  gather(year, value, -variable, -Variable, -line) |>
-  mutate(value = as.numeric(value)) |>
-  filter(!is.na(value)) |>
-  mutate(year = as.numeric(year))
+  tidy1()
 
 charge_interets <- T_7301 |>
   filter(line %in% c(56, 47)) |>
